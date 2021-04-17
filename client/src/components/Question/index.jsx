98f0c86a2ae6d7dd26468f820afdb267eaa313fe
Alan <you@example.comalanlichen1@gmail.com>
2021-04-16 17:46:12 -0700

    partially done with allingment

--- client/src/components/Question/index.jsx
+++ client/src/components/Question/index.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
-import { Box, Text, Center, Heading } from '@chakra-ui/react';
+import { Box, Text, Heading } from '@chakra-ui/react';
 import './Question.css';
 
 function Question(props) {
 	return (
 		<>
-			<Center>
-				<Box
-					p={5}
-					shadow="md"
-					borderWidth="1px"
-					borderRadius="lg"
-					width="25em"
-					className="questionBox"
-				>
-					<a href={`/question?q=${props.id}`}>
-						<Heading fontSize="xl">{props.subject}</Heading>
-					</a>
-					<Text mt={4} noOfLines={2}>
-						{props.question}
-					</Text>
-				</Box>
-			</Center>
+			<Box
+				p={5}
+				shadow="md"
+				borderWidth="1px"
+				borderRadius="lg"
+				width="25em"
+				className="questionBox"
+			>
+				<a href={`/question?q=${props.id}`}>
+					<Heading fontSize="xl">{props.subject}</Heading>
+				</a>
+				<Text mt={4} noOfLines={2}>
+					{props.question}
+				</Text>
+			</Box>
 			<br />
 		</>
 	);
--- client/src/pages/Main/index.jsx
+++ client/src/pages/Main/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Question from '../../components/Question';
-import { Text, Box, Flex, Spacer } from '@chakra-ui/react';
+import { Text, Box, Grid, GridItem } from '@chakra-ui/react';
 import { getQuestions } from '../../scripts/api';
 
 // function Main() {
@@ -44,27 +44,36 @@ class Main extends React.Component {
 				<Text fontSize="3xl">Recent questions</Text>
 				<br />
 				<br />
-				<Box
-					width="20em"
-					p={3}
-					color="white"
-					borderWidth="1px"
-					borderRadius="lg"
-					marginLeft="auto"
-					marginRight="20"
+
+				<Grid
+					w="100%"
+					h="100%"
+					gridTemplateColumns="repeat(3, 1fr)"
+					gridTemplateRows="1"
 				>
-					<Text fontSize="2xl">
-						<a href="/new">Add</a>
-					</Text>
-				</Box>
-				{this.state.questions.map(q => (
-					<Question
-						key={q._id}
-						subject={q.subject}
-						question={q.question}
-						id={q._id}
-					/>
-				))}
+					<GridItem colSpan={2}>
+						{this.state.questions.map(q => (
+							<Question
+								key={q._id}
+								subject={q.subject}
+								question={q.question}
+								id={q._id}
+							/>
+						))}
+					</GridItem>
+					<GridItem colSpan={2}>
+						<Box
+							width="20em"
+							p={3}
+							borderWidth="1px"
+							borderRadius="lg"
+						>
+							<Text fontSize="2xl">
+								<a href="/new">Add</a>
+							</Text>
+						</Box>
+					</GridItem>
+				</Grid>
 			</>
 		);
 	}
